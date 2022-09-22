b4d239098c9c5e2fd3555233cd4670c9ad2a90b9
Bruna Targino <contatobrunatargino@gmail.com>
2022-09-22 14:31:05 -0300

    adding tests

---
+++ cypress/e2e/todo.cy.js
@@ -0,0 +1,26 @@
+beforeEach(() => {
+  cy.visit('https://targ1no.github.io/todo-list/')
+});
+
+describe('Todo List', () => {
+  it('Clear input', () => {
+    cy.get('#input').type('School')
+    cy.get('#input').should('have.value', 'School')
+    cy.get('#clearButton').click()
+    cy.get('#input').should('not.have.value', 'School')
+  })
+
+  it('Create a Todo', () => {
+    cy.get('#input').type('School')
+    cy.get('#addButton').click()
+    cy.get('#list').should('have.length', 1)
+  })
+
+  it('Check Todo', () => {
+    
+  })
+
+  it('Delete Todo', () => {
+    
+  })
+})
