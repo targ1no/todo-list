f8712047ee3c32be25c1fc18d363754d504c3103
Bruna Targino <contatobrunatargino@gmail.com>
2022-10-08 20:00:33 -0300

    implementing bdd

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,38 +1,81 @@
-beforeEach(() => {
-  cy.visit('https://targ1no.github.io/todo-list/')
-});
+/* todo.cy.js */
 
-describe('Todo List', () => {
-  it('Clear input', () => {
-    cy.get('#input').type('School')
-    cy.get('#input').should('have.value', 'School')
-    cy.get('#clearButton').click()
+describe('Fluxo Todo List', () => {
+  context('Verificando botão de limpar texto no input', () => {
+    it('Dado que o usuário acessa a página da aplicação', () => {
+      cy.visit('https://targ1no.github.io/todo-list/')
+    });
 
-    cy.get('#input').should('not.have.value', 'School')
+    it('E digita um item no input', () => {
+      cy.get('#input')
+        .type('School')
+      cy.get('#input')
+        .should('have.value', 'School')
+    });
+
+    it('Quando clica no botão "clear"', () => {
+      cy.get('#clearButton')
+        .click()
+    });
+
+    it('Então o input deverá estar vazio', () => {
+      cy.get('#input')
+        .should('not.have.value', 'School')
+    })
   })
 
-  it('Create a Todo', () => {
-    cy.get('#input').type('School')
-    cy.get('#addButton').click()
+  context('Verificando botão para adicionar um novo Todo', () => {
+    it('Dado que o usuário acessa a página da aplicação', () => {
+      cy.visit('https://targ1no.github.io/todo-list/')
+    });
 
-    cy.get('#list').should('have.length', 1)
+    it('E digita um item no input', () => {
+      cy.get('#input')
+        .type('School')
+      cy.get('#input')
+        .should('have.value', 'School')
+    });
+
+    it('Quando clica no botão "Add"', () => {
+      cy.get('#addButton')
+        .click()
+    });
+
+    it('Então deverá ter criado um novo item na lista de Todos', () => {
+      cy.get('#list')
+        .should('have.length', 1)
+    })
   })
 
-  it('Check Todo', () => {
-    cy.get('#input').type('School')
-    cy.get('#addButton').click()
-    cy.get('#list').should('have.length', 1)
-    cy.get('[type=checkbox]').click()
-    
-    //asserts
-  })
+  context('Verificando checkbox para marcar Todo como feito', () => {
+    it('Dado que o usuário acessa a página da aplicação', () => {
+      cy.visit('https://targ1no.github.io/todo-list/')
+    });
 
-  it.only('Delete Todo', () => {
-    cy.get('#input').type('School')
-    cy.get('#addButton').click()
-    cy.get('#list').should('have.length', 1)
-    cy.get('.deleteItem').click()
-    
-    //asserts
+    it('E digita um item no input', () => {
+      cy.get('#input')
+        .type('School')
+      cy.get('#input')
+        .should('have.value', 'School')
+    });
+
+    it('E clica no botão "Add"', () => {
+      cy.get('#addButton')
+        .click()
+    });
+
+    it('Quando o Todo estiver criado', () => {
+      cy.get('#list')
+        .should('have.length', 1)
+    })
+
+    it('E clicar no checkbox em branco, acima do Todo', () => {
+      cy.get('[type=checkbox]')
+        .click()
+    })
+
+    it('Então o Todo deverá estar com um risco, apontando que já foi feito', () => {
+      //ASSERTIONS
+    })
   })
 })
